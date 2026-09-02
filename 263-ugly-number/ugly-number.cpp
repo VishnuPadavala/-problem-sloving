742bbc76c3cc44b44662d631f095d11f43f4db1a
Vishnu <vishnupadavala@gmail.com>
2026-09-02 15:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (77.12%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n==0)return 0;
+        if(n<=0)return 0;
         if(n==1)return 1;
         while(n%2==0) n/=2;
         while(n%3==0) n/=3;

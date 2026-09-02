9f20a73767f53bae57b04cc613978ad8cee9dba6
Vishnu <vishnupadavala@gmail.com>
2026-09-02 22:39:22 +0530

    Time: 97 ms (35.83%) | Memory: 111.7 MB (14.57%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.cpp
+++ 3373-maximum-prime-difference/maximum-prime-difference.cpp
@@ -1,11 +1,9 @@
-class Solution {
-public:
-    int maximumPrimeDifference(vector<int>& nums) {
-        int max_val=0;
-        for(int num:nums){
-            max_val=max(max_val,num);
-        }
-        vector<bool>prime(max_val+1,true);
+class primetable{
+    private:
+    int max_val=101;
+    vector<bool>prime=vector<bool>(101, true);
+    public:
+    primetable(){
         prime[0]=prime[1]=false;
         for(int i=2;i*i<=max_val;i++){
             if(!prime[i]){
@@ -15,9 +13,19 @@ public:
                 prime[j]=false;
             }
         }
+    }
+    bool check(int n){
+        return prime[n];
+    }
+};
+
+primetable p;
+class Solution {
+public:
+    int maximumPrimeDifference(vector<int>& nums) {
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(prime[nums[i]]){
+            if(p.check(nums[i])){
                 ans.push_back(i);
             }
         }
